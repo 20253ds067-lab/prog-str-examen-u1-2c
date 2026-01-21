d2a18ba2d3981c42fd618a506936c0f9d8e5a191
dxemely <20253ds093@utez.edu.mx>
2026-01-21 09:02:03 -0600

    subiendo

---
+++ U1GitConflicto/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static final String VERSION_OBJETIVO = "PENDIENTE";
+
+    public static void main(String[] args) {
+        public static final String VERSION_OBJETIVO = "merge";
+    }
+}
